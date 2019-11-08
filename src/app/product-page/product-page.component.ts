79988c79f964cd156456316e9430d3c4e065d840
papa28x4 <swissfair@yahoo.co.uk>
2019-11-08 17:47:07 +0100

    Adding the Tracklisting Component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-page',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
